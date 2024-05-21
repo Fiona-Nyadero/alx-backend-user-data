f072062ebaf7978a61c27e7ace8ab26837108c49
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-21 21:36:54 +0300

    Task 0 - fix pycodestyle

--- 0x01-Basic_authentication/SimpleAPI/api/v1/__init__.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 
 app_views = Blueprint('app_views', __name__)
 
+
 @app_views.route('/api/v1/status', methods=['GET'])
 def get_status() -> dict:
     """Return the API status."""
