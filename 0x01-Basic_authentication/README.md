d9d060ec3cb00731c753ed8fda5673b0d4a02974
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-22 02:37:37 +0000

    Reset

--- 0x01-Basic_authentication/README.md
+++
@@ -1,4 +0,0 @@
-0x01. Basic authentication
-
-This is the second alx project on handling user data:
-The following are the concepts:
--- 0x01-Basic_authentication/SimpleAPI/api/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-
-app_views = Blueprint('app_views', __name__)
-
-
-@app_views.route('/api/v1/status', methods=['GET'])
-def get_status() -> dict:
-    """Return the API status."""
-    return {'status': 'OK'}
