e1a39ea6ca96a53ef1bd3b9039ab23d508a4ddd8
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-23 21:26:22 +0300

    Task 4 - Require Auth method implementation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,19 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Checks for authorization'''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''Extracts Auth header from the request'''
