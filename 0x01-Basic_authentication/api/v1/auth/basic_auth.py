d5f5a374ee0d4fa4cd9688284e5fd823c0310c3d
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-26 03:56:06 +0300

    Task 7 - Extracts the Base64 part of header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     '''Inherits from the Auth Class'''
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        '''Returns the Base64 of the Authorization header'''
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header.startswith("Basic "):
+            return authorization_header[len("Basic "):]
+        return None
