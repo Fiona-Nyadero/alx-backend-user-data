197e8ae0708fca2ec53d807efb4094628e352d4d
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-28 20:40:47 +0300

    Task 5 - add a user with hashed pword

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,8 +2,32 @@
 """ Module for Auth"""
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a password for storing."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self, db: DB):
+        """Initialize an Auth instance."""
+        self._db = db
+
+    def register_user(self, email: str, password: str) -> User
+    """Register a user"""
+    try:
+        self._db.find_user_by(email=email)
+        raise ValueError(f"User {email} already exists")
+    except NoResultFound:
+        pass
+
+    pwd_hash = _hash_password(password)
+    user = self._db.add_user(email, pwd_hash)
+    return user
