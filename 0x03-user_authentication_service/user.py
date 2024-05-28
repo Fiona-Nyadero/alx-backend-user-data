a0c10cc8bf96fe320092a89664124463d80732e6
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-28 19:18:25 +0300

    Readme

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 '''Module implements the User class'''
 from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.orm import relationship
